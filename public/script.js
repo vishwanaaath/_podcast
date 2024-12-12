f535fa1894ede7b9bd3521e8164cf9d6deea55f3
vishwanaaath <vishwanathgowda2022@gmail.com>
2024-12-12 20:09:11 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,10 @@
+document.getElementById("button").addEventListener("click", () =>{
+  document.getElementById("button").style.display = "none";
+  document.getElementById("container").style.display = "flex";
+})
+
+
+
+
+
+ 
